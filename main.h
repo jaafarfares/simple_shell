bed898f8c3e53be94dcccc2a741912f28ee1d537
louay66 <louaylouay20@gmail.com>
2022-04-04 14:27:06 -0700

    test

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+char *read_line(void);
+#endif
