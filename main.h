bdfac57c77a2beea87a728268253a90992ba2e94
louay66 <louaylouay20@gmail.com>
2022-04-07 22:29:18 -0700

    main h

--- main.h
+++ main.h
@@ -1,6 +1,6 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-
+#ifndef MAIN_H
+#define MAIN_H
+#define unused  __attribute__((unused))
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -9,5 +9,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/wait.h>
+extern char **environ;
+int printenv(char **arg);
+char *_getenv(void);
+char **splitstr(char *input, char *delimiter);
+int prompt(void);
 char *read_line(void);
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+int _strncmp(char *s1, char *s2, size_t size);
 #endif
