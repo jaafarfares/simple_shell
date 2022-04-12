f59c3e3b2e846402edb4db5f18ff4fcf57dc3c9d
louay66 <louaylouay20@gmail.com>
2022-04-12 05:36:38 -0700

    built_in.c

---
+++ built_in.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *  * fun_builtin - check if the command passed is a builtin
+ *   * @command: string to check
+ *    *Return: -1 on failure 1 on success
+ *     */
+
+int fun_builtin(char **command)
+{
+	int i = 0;
+
+	if (_strcmp(command[0], "exit") == 0)
+	{
+		if (command[1] == NULL)
+		{
+			free(command);
+			exit(0);
+		}
+	}
+	if (_strcmp(command[0], "env") == 0)
+	{
+		i = printenv();
+		free(command);
+		return (i);
+	}
+	free(command);
+	return (0);
+}
